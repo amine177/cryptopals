b5ada549402083ec9392410f4dcfb9b94faaa905
aminos <amriamine@protonmail.com>
2018-08-05 18:00:15 +0100

    crytographic analysis of a single byte xor using frequency of high frequency egnlish chars

--- set1/singlebytexor.c
+++ set1/singlebytexor.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "hex2base64.h"
 
 
 struct bytescore {
   unsigned char byte;
   int score;
+  int firstindex;
 };
 
 struct bytescorer {
@@ -37,11 +39,11 @@ int score(char *s, struct bytescore *o)
     if (!found) {
       ptr->byte = byte;
       ptr->score = 1;
+      ptr->firstindex = i / 2;
       o[k++] = *ptr;
     }
   }
 
-  sort(o, k);
 
   return k;
 }
@@ -65,12 +67,30 @@ void sort(struct bytescore ptr[], int len)
   }
 }
 
-void analyse(char *s)
+unsigned char analyse(char *s)
 {
-  unsigned char key, byte;
+  unsigned char key = 0x0, res;
+  int max, len, sscore;
+  char *decoded = malloc(strlen(s)/2+1);
   struct bytescore *o = 
     (struct bytescore *) malloc(sizeof(struct bytescore) * strlen(s)/2);
-  int len = score(s, o);
+  len = score(s, o);
+  res = 0x0;
+  max = 0;
+  while (key != 0xff) {
+    sscore = 0;
+    for (int i = 0; i < len; i++)
+      if (isalpha(o[i].byte^key))
+        sscore += o[i].score;
+    if (sscore > max) {
+      max = sscore;
+      res = key;
+    }
+    key++;
+  }
+
+  return res;
+  /*
   printf("Analysing againsg etaoinshrdlu as frequent\
       english chars and the space...\n");
   char *letter = "etaoinshdrluk ";
@@ -79,21 +99,34 @@ void analyse(char *s)
     printf("\n\n");
     for (int i = 0; i < strlen(letter); i++) {
       key = o[0].byte ^ letter[i];
-      printf("key: %x, byte: %d\n", key, o[0].byte);
+      printf("key: %x, byte: %d, index: %d\n", key, o[0].byte, o[0].firstindex);
       for (int k = 0; k < strlen(s); k+=2) {
         byte = nibbletoi(*(s+k)) * 16 + nibbletoi(*(s+k+1));
-        printf("%c", byte ^ key);
+        char c = byte ^ key;
+        if (c == letter[i] && k/2 == o[0].firstindex)
+          alphan = 1;
+        decoded[k/2] = byte ^ key;
       }
+      if (alphan)
+        printf("potential solution(%d):\n%s\n", alphan, decoded);
+      alphan = 0;
       printf("\n");
       printf("\n\n");
     }
     letter = "ETAOINSHDRLUK ";
   }
+  */
 }
 
 int main(int argc, char *argv[])
 {
   char *s = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-  analyse(s);
-  getchar();
+  unsigned char key = analyse(s);
+  printf("Trying to resolve:\n");
+  for (int i = 0; i < strlen(s); i+=2) {
+    printf("%c", (nibbletoi(s[i])*16 + nibbletoi(s[i+1]))^key );
+  }
+  printf("\n");
+
+  return 0;
 }
